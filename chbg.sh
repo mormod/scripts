decedb3ff85e618cf2d3756091c7c3621cb7d10d
Moritz Reinhard Hüllmann <moritz.huellmann@rwth-aachen.de>
2021-12-02 19:14:53 +0100

    fuck all

--- chbg.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -z "$1" -a -f "$1" ] 
-then
-	ln -f -L $1 $HOME/.config/background
-	i3-msg restart
-else
-	echo "Please provide a path to your desired background."
-fi
-
--- init_desktop.sh
+++
@@ -1,6 +0,0 @@
-xrandr --output HDMI1 --auto --scale 1.5x1.5 --right-of eDP1
-feh --no-fehbg --bg-scale ~/.config/background
-~/.config/polybar/launch.sh
-
-# set keyboard layout of mech to eu
-xinput | grep -w 'SONiX USB DEVICE ' | awk '{print $5}' | tr '=' ' ' | awk '{print $2}' | xargs -I{} setxkbmap -device {} eu
