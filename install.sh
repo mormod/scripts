f1c7c5ceff0f43d1188fcfa01cc89064e4c4e205
Moritz Reinhard Hüllmann <moritz.huellmann@rwth-aachen.de>
2021-12-02 19:50:47 +0100

    pacman install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+pacman -Syy
+pacman -Sy --noconfirm base-devel rofi flameshot firefox htop keepassxc owncloud-client nautilus zathura neovim xcursor-breeze ttf-fira-code texlive-science texlive-science texlive-bin make cmake dunst cozy-audiobooks
+
