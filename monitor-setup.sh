0d5ac91530ae530ee5e093cdc0e20f3dcccacc67
Moritz Reinhard Hüllmann <moritz.huellmann@rwth-aachen.de>
2022-01-15 20:59:51 +0100

    Setup script for multi-monitor work
    Scales outputs appropriately and asigns workspaces

---
+++ monitor-setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+swaymsg output eDP-1 scale 1.5
+
+if [[ $(swaymsg -t get_outputs | grep -w DP-1) ]]; then
+	echo "DP-1 connected"
+	echo "Configuring it to be the main monitor"
+	swaymsg workspace 1 output DP-1	
+	swaymsg output DP-1 scale 1.3333333
+	swaymsg output DP-1 mode 2560x1440 pos 0 0
+	swaymsg output eDP-1 mode 2560x1440 pos 1920 0 
+else
+	swaymsg workspace 1 output eDP-1
+fi
+
