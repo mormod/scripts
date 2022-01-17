bc08a1fd58e350d4d75a75390cd15b08e5c2c6b5
Moritz Reinhard Hüllmann <moritz.huellmann@rwth-aachen.de>
2022-01-17 01:30:53 +0100

    Reformated and reworked status line

--- sway_status.sh
+++ sway_status.sh
@@ -3,26 +3,36 @@
 sep=' | '
 
 while true; do
-	output=""
-	weather=$(curl -s 'wttr.in/?format=%l:+%C+%t')
-	network_ssid=$(iwgetid -r)
-	battery_life=$(cat /sys/class/power_supply/BAT0/capacity)
-	battery_remaining=$(acpi | head -1 | awk '{ print $5 }')
-	time=$(date +'%c')
-	
-	if [[ ! -z "$network_ssid" ]]; then
-		output="${network_ssid}${sep}"
-	fi
-	
-	if [[ ! -z "$weather" ]]; then
-		output="${output}${weather}${sep}"
-	fi
-	
-	# Fixes problem with acpi showing the battery state for the wrong battery
-	if [[ "$battery_remaining" = "rate" ]]; then
-		battery_remaining=$(acpi | sed -n 2p | awk '{ print $5 }')
-	fi
+    output=""
+    weather=$(curl -s 'wttr.in/?format=%l:+%C+%t')
+    network_ssid=$(iwgetid -r)
+    battery_life=$(cat /sys/class/power_supply/BAT0/capacity)
+    battery_remaining_time=$(acpi | head -1 | awk '{ print $5 }')
+    time=$(date +'%c')
 
-	echo "${output}${battery_life}% (${battery_remaining})${sep}${time}${sep}"
-	sleep 1;	
+    if [[ ! -z "$network_ssid" ]]; then
+        output="${network_ssid}${sep}"
+    fi
+
+    if [[ ! -z "$weather" ]]; then
+        output="${output}${weather}${sep}"
+    fi
+
+    # Fixes problem with acpi showing the battery state for the wrong battery
+    if [[ "$battery_remaining" = "rate" ]]; then
+        battery_remaining_time=$(acpi | sed -n 2p | awk '{ print $5 }')
+    fi
+
+    output="${output}${battery_life}%"
+
+    # If laptop is fully charged and connected to power outlet, there is nothing to
+    # display, so only display if not empty
+    if [[ ! -z "$battery_remaining_time" ]]; then
+        output="${output} (${battery_remaining_time})"
+    fi
+
+    output="${output}${sep}"
+
+    echo "${output}${time}${sep}"
+    sleep 1;
 done
