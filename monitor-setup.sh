60bc24e54be0c2a988d0677f9d440eeb88e570ce
Moritz Reinhard Hüllmann <moritz.huellmann@rwth-aachen.de>
2022-02-01 11:07:52 +0100

    Monitor: Update setup script

--- monitor-setup.sh
+++ monitor-setup.sh
@@ -3,12 +3,14 @@
 swaymsg output eDP-1 scale 1.5
 
 if [[ $(swaymsg -t get_outputs | grep -w DP-1) ]]; then
-	echo "DP-1 connected"
-	echo "Configuring it to be the main monitor"
-	swaymsg workspace 1 output DP-1	
-	swaymsg output DP-1 scale 1.3333333
+	notify-send "DP-1 connected" "Configuring it to be the main monitor"
+	swaymsg workspace 1 output DP-1
+	swaymsg workspace 6 output DP-1
+    swaymsg workspace 7 output DP-1
+    swaymsg workspace 9 output eDP-1
+    swaymsg output DP-1 scale 1.3333333
 	swaymsg output DP-1 mode 2560x1440 pos 0 0
-	swaymsg output eDP-1 mode 2560x1440 pos 1920 0 
+	swaymsg output eDP-1 mode 2560x1440 pos 1920 0
 else
 	swaymsg workspace 1 output eDP-1
 fi
