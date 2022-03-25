8211daf095c1bdade17716357bfb525a969ddbeb
Moritz Reinhard Hüllmann <moritz.huellmann@rwth-aachen.de>
2022-03-25 23:26:39 +0100

    Remove TODO, generate man-pages in clean install

--- clean-install.sh
+++ clean-install.sh
@@ -161,6 +161,9 @@ curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | gpg -
 makepkg -si
 echo "Installed spotify"
 
+echo "Generating man-pages"
+sudo mandb
+
 ###########################################################################
 ### ZSH stuff #############################################################
 ###########################################################################
