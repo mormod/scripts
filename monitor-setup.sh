cf306ccdac2f7e7b19a7d5c6d07794c871f69541
Moritz Reinhard Hüllmann <moritz.huellmann@rwth-aachen.de>
2022-03-25 23:28:12 +0100

    Fix indentation

--- monitor-setup.sh
+++ monitor-setup.sh
@@ -5,7 +5,7 @@ swaymsg output eDP-1 scale 1.5
 if [[ $(swaymsg -t get_outputs | grep -w DP-1) ]]; then
 	notify-send "DP-1 connected" "Configuring it to be the main monitor"
 	swaymsg workspace 1 output DP-1
-  swaymsg output DP-1 scale 1.3333333
+	swaymsg output DP-1 scale 1.3333333
 	swaymsg output DP-1 mode 2560x1440 pos 0 0
 	swaymsg output eDP-1 mode 2560x1440 pos 1920 0
 else
