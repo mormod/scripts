6dab3e1ad545afffaf4377bed963e68a1fd76edd
Moritz Reinhard Hüllmann <moritz.huellmann@rwth-aachen.de>
2022-04-26 17:11:25 +0200

    Configure the monitor in the chair correctly

--- monitor-setup.sh
+++ monitor-setup.sh
@@ -2,13 +2,19 @@
 
 swaymsg output eDP-1 scale 1.5
 
+GETOUTPUTS=$(swaymsg -t get_outputs)
+
 if [[ $(swaymsg -t get_outputs | grep -w DP-1) ]]; then
+	# this is the monitor at my desk at home
 	notify-send "DP-1 connected" "Configuring it to be the main monitor"
-	swaymsg workspace 1 output DP-1
 	swaymsg output DP-1 scale 1.3333333
 	swaymsg output DP-1 mode 2560x1440 pos 0 0
 	swaymsg output eDP-1 mode 2560x1440 pos 1920 0
-else
-	swaymsg workspace 1 output eDP-1
 fi
 
+if [[ $(swaymsg -t get_outputs | grep -w HDMI-A-1) ]] && [[ $(swaymsg -t get_outputs | grep -w H9XZ401605) ]]; then
+	# this is the monitor in the chairs schlauchraum
+	notify-send "HDMI-A-1 connected" "Configuring it to be the main monitor"
+	swaymsg output HDMI-A-1 mode 1920x1200 pos 0 0
+	swaymsg output DP-1 mode 2560x1440 pos 1920 0
+fi
